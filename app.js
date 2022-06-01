9b744ffe95f72ebab61fecd0cd11237eefa87e4b
Farid Iskandarov <iskandarovfarid8@gmail.com>
2022-06-01 19:25:46 +0300

    Updating

--- app.js
+++ app.js
@@ -23,6 +23,28 @@ UI.prototype.addBookToList = function(book) {
     list.appendChild(row);
 }
 
+// Show Alert
+UI.prototype.showAlert = function(message, className) {
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = `alert ${className}`;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.container');
+    // Get form
+    const form = document.querySelector('#book-form');
+    // Insert alert
+    container.insertBefore(div, form)
+
+    // Timeout after 3 sec
+    setTimeout(function() {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
+
 // Clear Fields
 UI.prototype.clearFields = function() {
     document.getElementById('title').value = '';
@@ -45,12 +67,23 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
 
     // Instantiate UI
     const ui = new UI();
-    
-    // Add book to list
-    ui.addBookToList(book);
 
-    // Clear Fields
-    ui.clearFields();
+    // #3 Validate
+    if(title === '' || author === '' || isbn === '') {
+        // #3 Error alert
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
+
+        // #3 Show success
+        ui.showAlert('Book Added!', 'success');
+
+        // Clear Fields 
+        ui.clearFields();
+    }
+    
+    
 
     e.preventDefault();
 })
